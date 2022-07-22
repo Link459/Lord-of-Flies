ffad4d08ea2027018c6ff74336e227ffffc48cda
Link459 <97700587+Link459@users.noreply.github.com>
2022-07-22 20:37:49 +0200

    Add files via upload

---
+++ Lord.GameObjects.cs
@@ -0,0 +1,10 @@
+namespace Lord_of_Flies
+{
+ public class gos : MonoBehaviour
+ {
+    public GameObject AncientSword;
+
+    
+
+ }
+}
--- Lord.cs
+++ Lord.cs
@@ -2,18 +2,13 @@ namespace Lord_of_Flies
 {
     public class Lord : MonoBehaviour
     {
+        private HealthManager _hm;
+        private tk2dSpriteAnimator _anim;
+        private PlayMakerFSM _control;
+        private PlayMakerFSM _stuns;
+        private ParticleSystem _trail;
 
-             public static GameObject AncientSword { get; set; }
-     public static void Preloaded(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects){
-        AncientSword = UnityEngine.Object.Instantiate(preloadedObjects["sharedassets447"]["Shot HK Shadow(Clone)"]);
-        AncientSword.GetComponent<tk2dSprite>().color = new Color32(1,1,1,255);
-     }
-     
-        public HealthManager _hm;
-        public tk2dSpriteAnimator _anim;
-        public PlayMakerFSM _control;
-        public PlayMakerFSM _stuns;
-        public ParticleSystem _trail;
+        
         public void Awake()
         {
             _hm = gameObject.GetComponent<HealthManager>();
@@ -23,6 +18,7 @@ namespace Lord_of_Flies
         }
         public IEnumerator Start()  
         {
+            
          _trail = Trail.AddTrail(gameObject, 3, 0.8f, 1.5f, 1, 1.8f, Color.green);
          _hm.hp = 3000;
 
@@ -63,6 +59,8 @@ namespace Lord_of_Flies
             _anim.Library.GetClipByName("GSlash End").fps = 20;
             _anim.Library.GetClipByName("Cyclone").fps = 20;
             _anim.Library.GetClipByName("Fall").fps = 20;
+
+            _control.GetAction<FloatMultiply>("Slash Recover").multiplyBy = 5 / 6f;
             
             //modifing stuns
             _stuns.FsmVariables.GetFsmInt("Stun Combo").Value = 22;
@@ -70,24 +68,67 @@ namespace Lord_of_Flies
            //setting the modified attck points
            _control.GetState("").InsertCoroutine(4, (Func<IEnumerator>)SpinSlashLaunch,false);
 
-           _control.GetState("Attack1 S1").InsertMethod(0, ProjectileSpawner(() => AncientSword, 30f));
-           _control.GetState("Attack1 S2").InsertMethod(0, ProjectileSpawner(() => AncientSword, 30f));
-           _control.GetState("Attack1 S3").InsertMethod(0, ProjectileSpawner(() => AncientSword, 30f));
-           _control.GetState("Attack2 S1").InsertMethod(0, ProjectileSpawner(() => AncientSword, 30f));
-           _control.GetState("Attack2 S2").InsertMethod(0, ProjectileSpawner(() => AncientSword, 30f));
-           _control.GetState("Attack2 S3").InsertMethod(0, ProjectileSpawner(() => AncientSword, 30f));
-           _control.GetState("Attack2 S4").InsertMethod(0, ProjectileSpawner(() => AncientSword, 30f));
+           _control.GetState("Attack1 S1").InsertMethod(0, ProjectileSpawner(() => A(index), 30f));
+           _control.GetState("Attack1 S2").InsertMethod(0, ProjectileSpawner(() => AncientSword(index), 30f));
+           _control.GetState("Attack1 S3").InsertMethod(0, ProjectileSpawner(() => AncientSword(index), 30f));
+           _control.GetState("Attack2 S1").InsertMethod(0, ProjectileSpawner(() => AncientSword(index), 30f));
+           _control.GetState("Attack2 S2").InsertMethod(0, ProjectileSpawner(() => AncientSword(index), 30f));
+           _control.GetState("Attack2 S3").InsertMethod(0, ProjectileSpawner(() => AncientSword(index), 30f));
+           _control.GetState("Attack2 S4").InsertMethod(0, ProjectileSpawner(() => , 30f));
    
- 
-
+           //creating and setting the the Sword Spawn attck
+           _control.CreateState("Sword Spawn");
+           FsmState swordSpawn = _control.GetState("Sword Spawn");
+          /* _control.GetState("").AddAction("Fireball Pos", new GetDistance()
+           {
+           gameObject = new FsmOwnerDefault()
+           {
+           OwnerOption = OwnerDefaultOption.SpecifyGameObject,
+           GameObject = _control.gameObject
+           },
+           target = new FsmGameObject()
+           {
+            Value = HeroController.instance.gameObject
+           },
+           storeResult = ("Distance"),
+           everyFrame = true
+           });*/
+           swordSpawn.AddTransition("Jump","Sword Spawn");
+           
+           swordSpawn.AddCoroutine(SwordSpawn);
 
             yield break;
         }
 
-        //making the modified attacks;
+
+         //creating the new attacks
          IEnumerator SpinSlashLaunch()
         {
+            //spawns a random sword and makes it shoot at the player
+            for(int i = 0; i < 5;i++)
+            {
+                Instantiate(AncientSword1);
+                AncientSword1.transform.position = HeroController.instance.transform.position;
+                yield return new WaitForSeconds(0.2f);
+            }
             yield break;
+        }
+
+        IEnumerator SwordSpawn()
+        {
+            //spawns the swords in a half circle behind sly
+            float angle = 0;
+            for (int i = 0;i < 1;i++)
+            {
+            var direction = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle),0);
+            var go = Instantiate(AncientSword[index],transform.position,Quaternion.identity);// spawn your gameobject at the center
+            go.transform.position += 5 * direction; // spawn them 5 units away from the center
+            angle += Mathf.PI / 5; // for 10 objects in the half circle
+            }
+            yield return new WaitForSeconds(0.5f);
+            gameObject.transform.position = HeroController.instance.transform.position;
+            yield break;
+    
         }
     }
 }
